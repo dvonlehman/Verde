7931e3664bdea75442334c3def2e9f4f74143816
dvonlehman <david.vonlehman@gmail.com>
2012-06-21 15:02:36 -0700

    Making $.ajax request work for IE

--- Verde/QUnitTestGuiRenderer.cs
+++ Verde/QUnitTestGuiRenderer.cs
@@ -24,21 +24,14 @@ namespace Verde
 
             // Get the absolute URL of the request to the GUI. Just to be safe ensure that we have a trailing 
             // slash to ensure the final URL to qunit-css and qunit-script resolves correctly.
-            var url = new UriBuilder(context.Request.Url);
-            url.Query = string.Empty;
-            string absoluteUrl = url.Uri.ToString();
+            //var url = new UriBuilder(context.Request.Url);
+            //url.Query = string.Empty;
+            //string absoluteUrl = url.Uri.ToString();
 
             html = html.Replace("@@TITLE@@", Setup.CurrentSettings.GuiPageTitle);
             html = html.Replace("@@HEADER@@", Setup.CurrentSettings.GuiHeaderText);
-            html = html.Replace("@@PATH@@", url.ToString().TrimEnd('/'));
+            html = html.Replace("@@PATH@@", context.Request.Url.LocalPath.TrimEnd('/'));
             context.Response.Write(html);
-        }
-
-        private string EnsureNoTrailingSlath(string path)
-        {
-            if (path.EndsWith("/"))
-                path = path.Substring(0, path.Length - 1);
-            return path;
         }
     }
 }
