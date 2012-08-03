98746545325dc1485a4dc5734f7a6243fc51e32b
Maxim Novak <maxim.novak@gmail.com>
2012-08-03 15:53:07 +0300

    Client side support for custom message on Test Pass
    Assert.Pass("custom message")

--- MvcMusicStore/IntegrationTests/PassTests.cs
+++ MvcMusicStore/IntegrationTests/PassTests.cs
@@ -4,10 +4,16 @@ using Verde;
 namespace MvcMusicStore.IntegrationTests
 {
 	[IntegrationFixture]
-	public class GeneralTests
+	public class PassTests
 	{
 		[IntegrationTest]
 		public void Assert_Pass()
+		{
+			Assert.Pass();
+		}
+
+		[IntegrationTest]
+		public void Assert_Pass_CustomMessage()
 		{
 			Assert.Pass("Custom message");
 		}
